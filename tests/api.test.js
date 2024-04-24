841e65673bb100f3318a82138ba79e36560db12e
name <mkirilla@gmail.com>
2024-04-24 15:29:08 +0400

    tack/api-test | применение codestyle

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,80 +1,95 @@
 describe('Auth in bookstore', () => {
   test('Success user creation', async () => {
-    const response = await fetch('https://bookstore.demoqa.com/Account/v1/User', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
+    const response = await fetch(
+      'https://bookstore.demoqa.com/Account/v1/User',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          userName: 'polzovatel4',
+          password: 'passworD!1',
+        }),
       },
-      body: JSON.stringify({
-        "userName": "polzovatel4",
-        "password": "passworD!1"
-      })
-    })
-    expect(response.status).toBe(201);
-  });
+    )
+    expect(response.status).toBe(201)
+  })
 
   test('User is already exists', async () => {
-    const response = await fetch('https://bookstore.demoqa.com/Account/v1/User', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
+    const response = await fetch(
+      'https://bookstore.demoqa.com/Account/v1/User',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          userName: 'polzovatel4',
+          password: 'passworD!1',
+        }),
       },
-      body: JSON.stringify({
-        "userName": "polzovatel4",
-        "password": "passworD!1"
-      })
-    })
-    const data = await response.json();
+    )
+    const data = await response.json()
     expect(data.message).toBe('User exists!')
-    expect(response.status).toBe(406);
-  });
+    expect(response.status).toBe(406)
+  })
 
   test('Password does not meet the requirements', async () => {
-    const response = await fetch('https://bookstore.demoqa.com/Account/v1/User', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
+    const response = await fetch(
+      'https://bookstore.demoqa.com/Account/v1/User',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          userName: 'polzovatel4',
+          password: 'passw',
+        }),
       },
-      body: JSON.stringify({
-        "userName": "polzovatel4",
-        "password": "passw"
-      })
-    })
-    const data = await response.json();
-    expect(data.code).toBe("1300");
-    expect(response.status).toBe(400);
-  });
+    )
+    const data = await response.json()
+    expect(data.code).toBe('1300')
+    expect(response.status).toBe(400)
+  })
 
   test('Token successfuly created', async () => {
-    const response = await fetch('https://bookstore.demoqa.com/Account/v1/GenerateToken', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
+    const response = await fetch(
+      'https://bookstore.demoqa.com/Account/v1/GenerateToken',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          userName: 'polzovatel4',
+          password: 'passworD!1',
+        }),
       },
-      body: JSON.stringify({
-        "userName": "polzovatel4",
-        "password": "passworD!1"
-      })
-    })
-    const data = await response.json();
+    )
+    const data = await response.json()
     expect(data.result).toBe('User authorized successfully.')
-    expect(response.status).toBe(200);
-  });
-  
+    expect(response.status).toBe(200)
+  })
+
   test('Auth failed', async () => {
-    const response = await fetch('https://bookstore.demoqa.com/Account/v1/GenerateToken', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
+    const response = await fetch(
+      'https://bookstore.demoqa.com/Account/v1/GenerateToken',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          userName: 'polzovatel4',
+          password: 'passwo',
+        }),
       },
-      body: JSON.stringify({
-        "userName": "polzovatel4",
-        "password": "passwo"
-      })
-    })
-    const data = await response.json();
+    )
+    const data = await response.json()
     expect(data.status).toBe('Failed')
     expect(data.result).toBe('User authorization failed.')
-    expect(response.status).toBe(200);
-  });
-});
+    expect(response.status).toBe(200)
+  })
+})
